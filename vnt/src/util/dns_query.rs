df9a446e3e140cc87374848dd7caaf9caef810d0
cn1095 <1095053105@qq.com>
2024-12-18 16:05:30 +0800

    Update dns_query.rs

--- vnt/src/util/dns_query.rs
+++ vnt/src/util/dns_query.rs
@@ -238,27 +238,29 @@ fn check_for_redirect(domain: &String) -> anyhow::Result<Option<String>> {
                             let trimmed_location = location_str.trim_end_matches('/').to_string();
                             // 如果是新的重定向地址，更新 url，继续检查
                             url = trimmed_location.clone();
-                            println!("发现重定向地址: {}", url);
                             continue; // 继续下一次重定向请求
                         }
                     }
                 } else {
                     // 如果不是重定向状态码
                     if is_redirect {
-                        // 如果之前发生过重定向，则返回最终的地址
-                        println!("发现重定向地址: {}", url);
+                        // 如果之前发生过重定向，则返回最后获取到的重定向地址
                         return Ok(Some(url));
                     } else {
                         // 如果没有经历过重定向，则返回 None
-                        println!("没有发现重定向地址 {}", url);
                         return Ok(None);
                     }
                 }
             }
             Err(_) => {
                 // 发生任何错误时直接返回 Ok(None)，不抛出异常
-                println!("重定向地址异常 {}", url);
-                return Ok(None);
+                if is_redirect {
+                    // 如果之前发生过重定向，则返回最后获取到的重定向地址
+                      return Ok(Some(url));
+                 } else {
+                    // 如果没有经历过重定向，则返回 None
+                    return Ok(None);
+                }
             }
         }
     }
